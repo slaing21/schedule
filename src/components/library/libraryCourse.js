9a5d735b5a6eaf0616b69a7006b739d87b8a06aa
Stephen Laing <laingste000@alpine.local>
2018-07-25 09:28:19 -0600

    integrated fontawesome and built a custom icon component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Icon from '../icon';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label className="library-course__title">Problem Solving</label>
-                {/* icon component */}
+                { Icon('fas fa-check', 'library-course__icon') }
                 {/* arrow component */}
                 {/* action button component */}
+                { Icon('fas fa-plus-circle', 'library-course__action')}
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod sapien lacus, sed ultrices enim mollis a. Aliquam finibus eleifend ante, quis aliquam justo efficitur vitae.</p>
